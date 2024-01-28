60e6fe6fada0a11df5c873bba8e65deddb43181b
Saviour1001 <harshghodkar@gmail.com>
2024-01-28 23:30:26 +0530

    Harsh fixed everything

--- test.js
+++ test.js
@@ -6,54 +6,66 @@ import {
   clusterApiUrl,
 } from "@solana/web3.js";
 
-import workerDataReceived from 'worker_threads';
-console.log("Secret Key" + JSON.stringify(workerDataReceived));
-/* const swapUserKeypair = Keypair.fromSecretKey(new Uint8Array(workerDataReceived));
- console.log("Swap User : "+swapUserKeypair.publicKey.toBase58());
+import workerDataReceived from "worker_threads";
+// console.log("Secret Key" + JSON.stringify(workerDataReceived.workerData));
+const swapUserKeypair = Keypair.fromSecretKey(
+  new Uint8Array(workerDataReceived.workerData)
+);
+console.log("Swap User : " + swapUserKeypair.publicKey.toBase58());
 
-const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=13eff9dc-b9a2-4b0f-add6-c7a1ece4a7aa", "confirmed");
-let val = "https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112\&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v\&amount=100000000\&slippageBps=50";
+const connection = new Connection(
+  "https://mainnet.helius-rpc.com/?api-key=13eff9dc-b9a2-4b0f-add6-c7a1ece4a7aa",
+  "confirmed"
+);
+let val =
+  "https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112&outputMint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&amount=1000000&slippageBps=50";
+
+let quoteResponse;
 try {
-  const quoteResponse =await(await fetch(val)).json();
-console.log("Quote" + JSON.stringify(quoteResponse));
-
+  quoteResponse = await (await fetch(val)).json();
+  console.log("found the quote");
+  console.log("Quote" + JSON.stringify(quoteResponse));
 } catch (e) {
   console.log("Error is " + e);
-} */
+}
 
 // get serialized transactions for the swap
 const { swapTransaction } = await (
- await fetch("https://quote-api.jup.ag/v6/swap", {
-   method: "POST",
-   headers: {
-     "Content-Type": "application/json",
-   },
-   body: JSON.stringify({
-     // quoteResponse from /quote api
-     quoteResponse,
-     // user public key to be used for the swap
-     userPublicKey: swapUserKeypair.publicKey.toString(),
-     // auto wrap and unwrap SOL. default is true
-     wrapAndUnwrapSol: true,
-     // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
-     // feeAccount: "fee_account_public_key"
-   }),
- })
+  await fetch("https://quote-api.jup.ag/v6/swap", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // quoteResponse from /quote api
+      quoteResponse,
+      // user public key to be used for the swap
+      userPublicKey: swapUserKeypair.publicKey.toString(),
+      // auto wrap and unwrap SOL. default is true
+      wrapAndUnwrapSol: true,
+      // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
+      // feeAccount: "fee_account_public_key"
+    }),
+  })
 ).json();
+
+// console.log("Swap Transaction" + swapTransaction);
 
 // deserialize the transaction
 const swapTransactionBuf = Buffer.from(swapTransaction, "base64");
-var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-console.log("transaction:"+transaction+"Conn"+connection);
 
+var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
+
+console.log("////////////////");
+// console.log("Swap Transaction" + JSON.stringify(transaction));
 // sign the transaction
-/* transaction.sign([swapUserKeypair._keypair]);
+transaction.sign([swapUserKeypair]);
 
 const rawTransaction = transaction.serialize();
+
+console.log("Raw Transaction" + rawTransaction);
 const txid = await connection.sendRawTransaction(rawTransaction, {
   skipPreflight: true,
 });
 await connection.confirmTransaction(txid);
 console.log(`https://solscan.io/tx/${txid}`);
-
- */
--- testingWorker.js
+++ testingWorker.js
@@ -1,22 +1,22 @@
-import { Worker, isMainThread, workerData, parentPort } from 'worker_threads';
+import { Worker, isMainThread, workerData, parentPort } from "worker_threads";
 import keys from "./output";
 
 /* const totalWorkers = require('os').cpus().length - 2;
 console.log(`Running with ${totalWorkers} worker(s).`); */
 
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 1; i++) {
   const worker = new Worker("./test.js", { workerData: keys[i] });
 
-  worker.on('message', async (result) => {
+  worker.on("message", async (result) => {
     console.log(result);
-        console.log("Am In")
+    console.log("Am In");
   });
 
-  worker.on('error', (error) => {
-    console.error('Worker error:', error);
+  worker.on("error", (error) => {
+    console.error("Worker error:", error);
   });
 
-  worker.on('exit', (code) => {
+  worker.on("exit", (code) => {
     if (code !== 0) {
       console.error(`Worker stopped with exit code ${code}`);
     }
