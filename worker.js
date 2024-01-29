3749c166b0c3f7bd52740cd2a7a823502561b071
Saviour1001 <harshghodkar@gmail.com>
2024-01-30 00:54:47 +0530

    Fixed Shubh's bad script :tada:

--- worker.js
+++ worker.js
@@ -8,44 +8,44 @@ import {
 
 import workerDataReceived from "worker_threads";
 
-import { RPC_URLS, amount, inputToken, outputToken, slippage } from "./consts.js";
+import {
+  RPC_URLS,
+  amount,
+  inputToken,
+  outputToken,
+  slippage,
+} from "./consts.js";
 // console.log("Secret Key" + JSON.stringify(workerDataReceived.workerData));
 
 let transactions = [];
 // console.log("Data Received :" + JSON.stringify(workerDataReceived.workerData.t1))
-transactions.push(workerDataReceived.workerData.t1,workerDataReceived.workerData.t2,workerDataReceived.workerData.t3)
-  let counter = 0;
-  for (let i = 0; i < 3; i++) {
-    console.log("Executing");
-    try {
-      console.log("//////sending the txn with RPC 1//////////");
-      const connection = new Connection(RPC_URLS[counter], "confirmed");
-      const txid = await connection.sendRawTransaction(transactions[i], {
-        skipPreflight: true,
-      });
-      console.log("//////confirming the txn//////////");
-      await connection.confirmTransaction(txid);
+transactions.push(
+  workerDataReceived.workerData.t1,
+  workerDataReceived.workerData.t2,
+  workerDataReceived.workerData.t3
+);
 
-      console.log(`https://solana.fm/tx/${txid}`);
-      counter++;
-      if (counter == 4) {
-        counter = 0;
-      }
-    } catch (e) {
-      console.log("//////sending the txn with RPC 1//////////");
-      const connection = new Connection(RPC_URLS[counter + 1], "confirmed");
-      const txid = await connection.sendRawTransaction(transactions[i], {
-        skipPreflight: true,
-      });
-      console.log("//////confirming the txn//////////");
-      await connection.confirmTransaction(txid);
+for (let i = 0; i < 3; i++) {
+  console.log("Executing");
 
-      console.log(`https://solana.fm/tx/${txid}`);
-      console.log("Error is " + e);
-    }
-  }
+  const connection1 = new Connection(RPC_URLS[0], "confirmed");
+  const connection2 = new Connection(RPC_URLS[1], "confirmed");
+  const connection3 = new Connection(RPC_URLS[2], "confirmed");
 
+  console.log("Sending via RPC url", RPC_URLS[0]);
+  const txid1 = await connection1.sendRawTransaction(transactions[i], {
+    skipPreflight: true,
+  });
 
+  console.log("Sending via RPC url", RPC_URLS[1]);
+  const txid2 = await connection2.sendRawTransaction(transactions[i], {
+    skipPreflight: true,
+  });
 
+  console.log("Sending via RPC url", RPC_URLS[2]);
+  const txid3 = await connection3.sendRawTransaction(transactions[i], {
+    skipPreflight: true,
+  });
 
-
+  console.log("txid is " + txid1);
+}
